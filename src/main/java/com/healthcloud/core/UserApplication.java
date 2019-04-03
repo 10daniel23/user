99a1cb969b8a74eb017917298807a36ec9a1fabf
danie <danie@192.168.1.4>
2019-04-03 20:33:15 -0300

    3 commit desde mi pc (es el 4to en realidad.

--- src/main/java/com/healthcloud/core/UserApplication.java
+++ src/main/java/com/healthcloud/core/UserApplication.java
@@ -13,6 +13,8 @@ public class UserApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(UserApplication.class, args);
 		//Es una prueba de un nuevo commit
+		
+		//QUe pasa ahora?
 	}
 
 }
